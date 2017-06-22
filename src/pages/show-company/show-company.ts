782d677f067b0588a9c5ee9cd0e6e08c7aedd60a
jatin7gupta <jatin13csu055@ncuindia.edu>
2017-06-22 23:41:51 +0530

    FIXED: Date I/O

--- src/pages/show-company/show-company.ts
+++ src/pages/show-company/show-company.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import {combineAll} from "rxjs/operator/combineAll";
 import {ShowRegistrationsPage} from '../show-registrations/show-registrations';
 
 
@@ -12,7 +11,10 @@ import {ShowRegistrationsPage} from '../show-registrations/show-registrations';
 export class ShowCompanyPage {
   company: any;
   test: any;
-  myDate: any;
+  ionicDate: any;
+  ionicDateDisp: any;
+  editValue = true;
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.company = navParams.get('company');
@@ -21,15 +23,36 @@ export class ShowCompanyPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ShowCompanyPage');
+
+  }
+  ngOnInit() {
+    let parts =this.company.date_of_Placement.split('-'); //MM-DD-YYYY
+    // month (parts[0]), Javascript counts months from 0: January - 0, February - 1, etc
+    this.ionicDate = new Date(parts[2], parts[0]-1, parts[1]);
+    this.ionicDateDisp = this.ionicDate.toISOString();
+    console.log(this.ionicDate);
   }
   saveCompany() {
-    console.log('save');
-    console.log(this.myDate);
+    let date = new Date(this.ionicDateDisp);
+     console.log(date.getMonth()+1);
+     console.log(date.getDate());
+     console.log(date.getFullYear());
+    if(this.editValue == false) {
+      //perform database modify
+    }
+    else {
+
+    }
+  }
+  onEditClick(){
+    this.editValue = false;
+
   }
   showRegistrations(company) {
     this.navCtrl.push(ShowRegistrationsPage, {
       company: company
     });
   }
+
 
 }
--- src/pages/show-student/show-student.ts
+++ src/pages/show-student/show-student.ts
@@ -9,6 +9,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ShowStudentPage {
   student: any;
+  editValue = true;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.student = navParams.get('student');
   }
@@ -16,5 +17,15 @@ export class ShowStudentPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad ShowStudentPage');
   }
+  onEditClick(){
+    this.editValue = false;
+  }
+  saveStudent() {
+    if(this.editValue == false) {
+      //perform database modify
+    }
+    else {
 
+    }
+  }
 }
